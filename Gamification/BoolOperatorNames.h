dea86b7093cba4b11f77c6db53b0460297f81451
Joseph Collado <sourpina@gmail.com>
2024-12-02 02:50:25 -0500

    Create BoolOperatorNames.h
    Factored out the BookOperator enum into its own header file for use in multiple classes

---
+++ Gamification/BoolOperatorNames.h
@@ -0,0 +1,14 @@
+#ifndef BOOLOPERATORNAMES_H
+#define BOOLOPERATORNAMES_H
+
+enum BoolOperator {
+	None,
+	LessThan,
+	LessThanOrEqualTo,
+	GreaterThan,
+	GreaterThanOrEqualTo,
+	NotEqualTo,
+	EqualTo
+};
+
+#endif
